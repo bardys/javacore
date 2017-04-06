a9997cdf2a876b20834c86f29370b940cff6d41a
Andrii Bardys <a.bardys@gmail.com>
2017-04-06 18:18:25 +0300

    lesson 6 homework done

--- src/main/java/com/abardys/runners/homework/Lesson6Runner.java
+++ src/main/java/com/abardys/runners/homework/Lesson6Runner.java
@@ -1,7 +1,12 @@
 package com.abardys.runners.homework;
 
+import com.abardys.runners.CalculateCircleAreaRunner;
+import com.abardys.runners.CompareCircleAreaRunner;
+import com.abardys.runners.TriangleRunner;
+
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Created by anbar on 03-Apr-17.
@@ -54,7 +59,7 @@ public class Lesson6Runner {
 
         int[] cArray = new int[15];
         for(int i=0; i < cArray.length; i++){
-            int  nC = randC.nextInt(9);
+            int  nC = randC.nextInt(10);
             cArray[i] = nC;
         }
 
@@ -76,7 +81,7 @@ public class Lesson6Runner {
 
         int[] dArray = new int[15];
         for(int i=0; i < dArray.length; i++){
-            int  nD = randD.nextInt(999);
+            int  nD = randD.nextInt(1000);
             dArray[i] = nD;
         }
 
@@ -98,5 +103,66 @@ public class Lesson6Runner {
         System.out.println("min: " + minD);
         System.out.println("max: " + maxD);
 
+        System.out.println("==========================================================");
+
+        Random randE = new Random();
+
+        int[][] eArray = new int[8][5];
+        for(int j=0; j < eArray.length; j++){
+            for (int k=0; k<eArray[j].length; k++) {
+                int nE = randE.nextInt(90) + 10;
+                eArray[j][k] = nE;
+            }
+        }
+
+        for(int j=0; j < eArray.length; j++) {
+            for (int k = 0; k < eArray[j].length; k++) {
+                System.out.print(eArray[j][k] + " ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("==========================================================");
+
+        Random randG = new Random();
+
+        int[][] gArray = new int[8][5];
+        for(int j=0; j < gArray.length; j++){
+            for (int k=0; k<gArray[j].length; k++) {
+                int nG = randG.nextInt(999) + 1;
+                gArray[j][k] = nG;
+            }
+        }
+
+        for(int j=0; j < gArray.length; j++) {
+            for (int k = 0; k < gArray[j].length; k++) {
+                if(gArray[j][k] < 10){
+                System.out.print(" " + " " + gArray[j][k] + " ");}
+                else if(gArray[j][k] < 100 && gArray[j][k] > 9){
+                    System.out.print(" " + gArray[j][k] + " ");}
+                else{
+                    System.out.print(gArray[j][k] + " ");}
+            }
+            System.out.println("");
+        }
+
+        System.out.println("==========================================================");
+
+        int[] hArray = new int[15];
+        try {
+            for (int i = 0; i < hArray.length; i++) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter number or write 'exit': ");
+                int choice = scanner.nextInt();
+                hArray[i] = choice;
+            }
+        }
+        catch (Exception e) {
+            int sum = 0;
+            for (int i = 0; i < hArray.length; i++) {
+                sum = sum + hArray[i];
+            }
+            System.out.println(sum);
+        }
     }
 }
